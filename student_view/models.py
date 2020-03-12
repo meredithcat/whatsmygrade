278692547fa9a482636362df9f1cfb087384a2fd
Meredith Murphy <meredithcat@gmail.com>
2020-03-12 11:21:03 -0700

    changes for postgres

--- student_view/models.py
+++ student_view/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+  name = models.CharField(max_length=50)
+  sid = models.CharField(max_length=50)
+  assignments = models.ManyToManyField('Assignment', through='StudentAssignment')
+
+  def __str__(self):
+    return self.name
+
+class Assignment(models.Model):
+  name = models.CharField(max_length=50)
+  max_points = models.IntegerField()
+
+  def __str__(self):
+    return self.name
+
+class StudentAssignment(models.Model):
+  student = models.ForeignKey('Student', on_delete=models.CASCADE)
+  assignment = models.ForeignKey('Assignment', on_delete=models.CASCADE)
+  points = models.DecimalField(max_digits=5, decimal_places=2)
+  hours_late = models.DecimalField(max_digits=5, decimal_places=2)
+
+  def __str__(self):
+    return f'Student {self.student} assignment {self.assignment}'
